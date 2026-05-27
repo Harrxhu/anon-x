254ef44eefcf1070b43b692a27eefc852297e5ce
Harrxhu <suniyoff7@gmail.com>
2026-05-27 23:43:17 +0530

    final port fix

--- app.py
+++ app.py
@@ -42,4 +42,6 @@ def message(data):
     )
 
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
