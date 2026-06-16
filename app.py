211b523b1a8c95990b9f4c3eab23f5d7d7f10bfa
Harrxhu <suniyoff7@gmail.com>
2026-06-16 23:42:19 +0530

    added disconnect event and call signaling

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
@@ -6,16 +6,26 @@ app.config["SECRET_KEY"] = "secret"
 
 socketio = SocketIO(app)
 
+# CONNECTED USERS
+users = {}
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
 # JOIN ROOM
+
 @socketio.on("join_room")
 def join(data):
 
     username = data["username"]
     room = data["room"]
+
+    users[request.sid] = {
+        "username": username,
+        "room": room
+    }
 
     join_room(room)
 
@@ -28,7 +38,9 @@ def join(data):
         room=room
     )
 
+
 # SEND MESSAGE
+
 @socketio.on("send_message")
 def message(data):
 
@@ -40,6 +52,29 @@ def message(data):
         },
         room=data["room"]
     )
+
+
+# USER DISCONNECTED
+
+@socketio.on("disconnect")
+def disconnect_user():
+
+    if request.sid in users:
+
+        username = users[request.sid]["username"]
+        room = users[request.sid]["room"]
+
+        emit(
+            "receive_message",
+            {
+                "username": "SYSTEM",
+                "message": f"{username} disconnected"
+            },
+            room=room
+        )
+
+        del users[request.sid]
+
 
 # WEBRTC OFFER
 
@@ -53,6 +88,7 @@ def offer(data):
         include_self=False
     )
 
+
 # WEBRTC ANSWER
 
 @socketio.on("answer")
@@ -64,6 +100,7 @@ def answer(data):
         room=data["room"],
         include_self=False
     )
+
 
 # ICE CANDIDATE
 
@@ -77,7 +114,15 @@ def ice_candidate(data):
         include_self=False
     )
 
+
 if __name__ == "__main__":
+
     import os
+
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host="0.0.0.0", port=port)
+
+    socketio.run(
+        app,
+        host="0.0.0.0",
+        port=port
+    )
